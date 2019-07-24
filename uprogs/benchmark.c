3be17dce37f8ced543c2610358f89f256861c122
H Paterson <patha454@student.otago.ac.nz>
2019-07-24 14:49:49 +1200

    Add fib benchmark

--- uprogs/benchmark.c
+++ uprogs/benchmark.c
@@ -3,6 +3,34 @@
 #include "user.h"
 
 #define FORK_BENCH 'f'
+#define FIB_BENCH 'i'
+
+#define FIB_N 30
+
+int fib(int n) {
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  return fib(n - 1) + fib (n - 2);
+}
+
+int fib_bench(int n) {
+  int i;
+  int pid;
+  int start = time();
+  for (i = 0; i < n; i++) {
+    pid = fork();
+    if (!pid) {
+      fib(FIB_N);
+      exit();
+    }
+  }
+  for (i = 0; i < n; i++) {
+    wait();
+  }
+  int end = time();
+  return end - start;
+}
 
 int fork_bench(int n) {
   int i;
@@ -33,8 +61,11 @@ int main(int argc, char* argv[]) {
   switch (benchmark) {
   case FORK_BENCH:
     score = fork_bench(n);
-    printf(1, "Benchmark Fork %d score: %duS\n", n, score);
+    printf(1, "Benchmark Fork %d score: %d uS\n", n, score);
     break;
+  case FIB_BENCH:
+    score = fib_bench(n);
+    printf(1, "Benchmark Fork %d score: %d uS\n", n, score);
   default:
     break;
   }
