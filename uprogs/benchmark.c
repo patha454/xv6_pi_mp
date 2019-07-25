c6c6697f20dad17e1f23144c758bf288de3ba71f
H Paterson <patha454@student.otago.ac.nz>
2019-07-25 12:15:26 +1200

    Finished xv6 microbenchmarks

--- uprogs/benchmark.c
+++ uprogs/benchmark.c
@@ -10,23 +10,21 @@
 #define SUM_FILE "benchmark"
 #define SUM_BUF_SIZE 1000000
 
-int sum_bench() {
+void sum() {
   int i = 1;
   int sum = 0;
   int len;
   int pid;
   char* buf;
-  int start = time();
   int fd = open(SUM_FILE, 0);
   buf = malloc(SUM_BUF_SIZE);
   len = read(fd, buf, SUM_BUF_SIZE);
-  while (i + 1 * len < SUM_BUF_SIZE) {
+  while ((i + 1) * len < SUM_BUF_SIZE) {
     memmove((void*) (buf + i * len), (void*) buf, len);
     i++;
   }
-  printf(1, "good copy\n");
   pid = fork();
-  if (pid) {
+  if (!pid) {
     for (i = 0; i < SUM_BUF_SIZE / 2; i++) {
       sum += buf[i];
     }
@@ -35,10 +33,26 @@ int sum_bench() {
     for (i = SUM_BUF_SIZE / 2; i < SUM_BUF_SIZE; i++) {
       sum += buf[i];
     }
-    wait();
   }
+  wait();
+  return;
+}
+
+int sum_bench(int n) {
+  int start = time();
+  int i;
+  int pid;
+  for (i = 0; i < n; i++) {
+    pid = fork();
+    if (!pid) {
+      sum();
+      exit();
+    }
+  }
+  for (i = 0; i < n; i++) {
+    wait();
+  } 
   return time() - start;
-  
 }
 
 int fib(int n) {
@@ -103,7 +117,7 @@ int main(int argc, char* argv[]) {
     break;
   case SUM_BENCH:
     score = sum_bench(n);
-    printf(1, "Benchmark sum %d score: %d uS\n", n, score);
+    printf(1, "Benchmark Sum %d score: %d uS\n", n, score);
     break;
   default:
     break;
